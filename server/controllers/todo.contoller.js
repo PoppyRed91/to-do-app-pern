a17a7ca2f4e231469ceb9ad9cc99d92ba2c9c402
PoppyRed91 <pfercek@gmail.com>
2024-04-03 13:18:40 +0200

    typo status

--- server/controllers/todo.contoller.js
+++ server/controllers/todo.contoller.js
@@ -103,7 +103,7 @@ exports.getAllTodos = async (req, res) => {
   try {
     const todos = await pool.query("SELECT * FROM todos");
     res.status(200).json({
-      staus: "success",
+      status: "success",
       result: todos.rowCount,
       data: { todos: todos.rows },
     });
@@ -124,12 +124,12 @@ exports.getTodo = async (req, res) => {
     const todo = await pool.query("SELECT * FROM todos WHERE id=$1", [id]);
     if (todo.rowCount === 0) {
       return res.status(404).json({
-        staus: "fail",
+        status: "fail",
         message: `Todo with id ${id} not found`,
       });
     }
     res.status(200).json({
-      staus: "success",
+      status: "success",
       data: { todo: todo.rows[0] },
     });
   } catch (error) {
@@ -149,7 +149,7 @@ exports.getUserTodos = async (req, res) => {
       id,
     ]);
     res.status(200).json({
-      staus: "success",
+      status: "success",
       result: todos.rowCount,
       data: { todos: todos.rows },
     });
