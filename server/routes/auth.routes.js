57a80ce75b70cfc847a33e60d0b0f5908b1b52be
PoppyRed91 <pfercek@gmail.com>
2024-04-05 18:47:46 +0200

    Revert "autentication and jsonWT fix"
    This reverts commit 3c22692cf50219c5ce74c41ff28154205f58c63d.

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,37 +1,28 @@
 const express = require("express");
+
+/*
+This line imports the authController object from the ../controllers/auth.controller file. 
+This object contains functions to handle signup and login logic.
+*/
 const authController = require("../controllers/auth.controller");
+
+/*
+creates a new instance of the Express router, which is used to define routes for the application.
+*/
 const router = express.Router();
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-const pool = require("../db");
-const verifyToken = require("../middleware/auth.middleware");
+
+/*
+This line defines a POST route for the "/signup" endpoint. When a POST request is made to this endpoint, 
+Express will execute the signup function from the authController.
+This line defines a POST route for the "/login" endpoint. When a POST request is made to this endpoint, 
+Express will execute the login function from the authController.
+*/
 
 router.post("/signup", authController.signup);
-// Login route with JWT verification middleware
-router.post("/login", verifyToken, async (req, res) => {
-  const { email, password } = req.body;
+router.post("/login", authController.login);
 
-  try {
-    const users = await pool.query("SELECT * FROM users WHERE email = $1", [
-      email,
-    ]);
-    if (!users.rows.length)
-      return res.status(404).json({ detail: "User does not exist." });
-
-    const success = await bcrypt.compare(
-      password,
-      users.rows[0].hashed_password
-    );
-    if (!success) return res.status(401).json({ detail: "Login failed" });
-
-    // If JWT verification passed, proceed with login logic
-    const token = jwt.sign({ email }, process.env.JWT_SECRET, {
-      expiresIn: "1hr",
-    });
-    res.json({ email: users.rows[0].email, token });
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send("Server Error");
-  }
-});
+/*
+This line exports the router object, making it available for use in other parts of the application. 
+It allows the defined routes to be mounted as middleware.
+*/
 module.exports = router;
